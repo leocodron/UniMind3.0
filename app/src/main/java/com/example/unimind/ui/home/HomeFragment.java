e7c8996d7259edd8f3ebaca5c919cf6def66ce59
lewisdean <lewisdean2010@gmail.com>
2021-02-06 15:07:31 +0000

    Added text stuff 2.

--- app/src/main/java/com/example/unimind/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/unimind/ui/home/HomeFragment.java
@@ -10,10 +10,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.unimind.R;
 import com.example.unimind.ui.gethelp.GetHelp;
