bbdaa65cc406846f42d195b40cc5ca1a2961ac7c
lewisdean <lewisdean2010@gmail.com>
2021-02-06 14:57:10 +0000

    Added text stuff

--- app/src/main/java/com/example/unimind/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/unimind/ui/home/HomeFragment.java
@@ -1,12 +1,9 @@
 package com.example.unimind.ui.home;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,48 +13,23 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.unimind.R;
-import com.example.unimind.ui.gethelp.GetHelp;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-    private Button getHelpButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.HomeTitle);
-        textView.setText("UniMind");
-        textView.setGravity(1);
-
-        System.out.println(R.id.HomeGetHelp);
-
-
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-
-        Button getHelpButton = (Button) view.findViewById(R.id.HomeGetHelp);
-//        Log.d("MyApp",getHelpButton.toString());
-
-        getHelpButton.setOnClickListener(new View.OnClickListener()
-        {
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
-            public void onClick(View v)
-            {
-                System.out.println("test");
-                Log.d("Test","I am here");
-
-                moveToGetHelp();
-
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
             }
         });
-
-
         return root;
     }
-
-    public void moveToGetHelp() {
-        this.startActivity(new Intent(this.getActivity(), GetHelp.class));
-    }
-
 }
--- app/src/main/java/com/example/unimind/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/unimind/ui/home/HomeViewModel.java
@@ -1,22 +1,20 @@
 package com.example.unimind.ui.home;
 
-import android.view.Gravity;
-import android.widget.TextView;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class HomeViewModel extends ViewModel {
 
-    private TextView homeTitle;
+    private MutableLiveData<String> mText;
 
     public HomeViewModel() {
-//        homeTitle = new TextView();
-
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
     }
 
-    public TextView getText() {
-        return homeTitle;
+    public LiveData<String> getText() {
+        return mText;
     }
 }
+
